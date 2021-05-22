50dfd821a77cbaea3b02be813c4f6a01fda16a3f
Maxmilite <yufe008@live.com>
2021-05-22 22:33:35 +0800

    Updated ver1.1

--- LuoguEmojiSender.js
+++ LuoguEmojiSender.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LuoguEmojiSender
 // @namespace    https://github.com/Maxmilite/LuoguEmojiSender
-// @version      1.0
+// @version      1.1
 // @description  一款可以帮助您在洛谷轻松发送 QQ 表情信息的插件.
 // @author       Maxmilite
 // @match        https://www.luogu.com.cn/*
@@ -14,6 +14,7 @@
     
     // 此项定义前后缀功能，用于表情的识别，以默认配置为例
     // 如果在此配置下，当且仅当输入的内容为大括号包裹的qq表情代码（即 "{/代码}"）时才会进行替换操作。
+    // 当然，您可以直接将其设置为空字符串，来达到无缝衔接的效果
     const prefix = "{", suffix = "}";
 
     // 此处为用户个性化设置区，输入格式按照 JSON 格式输入
@@ -25,6 +26,10 @@
     }
     
     // -------------------------上方为用户修改配置区--------------------------------
+
+    // 1.1 更新内容：
+    // 优化操作逻辑，增加用户配置区
+
     const replaceElement = {
         "/qq": "![/qq](https://cdn.luogu.com.cn/upload/pic/62224.png)",
         "/cy": "![/cy](https://cdn.luogu.com.cn/upload/pic/62225.png)",
@@ -46,6 +51,27 @@
     };
     
     document.addEventListener("keypress", function () {
+        for (let i in replaceElement) {
+            let newString = prefix + i + suffix;
+            if (typeof markdownPalettes != "undefined") {
+                markdownPalettes.content = markdownPalettes.content.replaceAll(newString, replaceElement[i]);
+            }
+            if (document.getElementById("feed-content") != null) {
+                document.getElementById("feed-content").value = document.getElementById("feed-content").value.replaceAll(newString, replaceElement[i]);
+            }
+        }
+        for (let i in userElement) {
+            let newString = prefix + i + suffix;
+            if (typeof markdownPalettes != "undefined") {
+                markdownPalettes.content = markdownPalettes.content.replaceAll(newString, userElement[i]);
+            }
+            if (document.getElementById("feed-content") != null) {
+                document.getElementById("feed-content").value = document.getElementById("feed-content").value.replaceAll(newString, userElement[i]);
+            }
+        }
+    })
+
+    document.addEventListener("click", function () {
         for (let i in replaceElement) {
             let newString = prefix + i + suffix;
             if (typeof markdownPalettes != "undefined") {
