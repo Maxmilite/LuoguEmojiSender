ec5a922fd5823c78bd96fcb4731e3c32cf28ef40
Maxmilite <yufe008@live.com>
2021-05-22 21:17:20 +0800

    Added Emoji list

--- LuoguEmojiSender.js
+++ LuoguEmojiSender.js
@@ -10,13 +10,30 @@
 
 (function() {
     const replaceElement = {
-        "/qq": "![/qq](https://cdn.luogu.com.cn/upload/pic/62224.png)"
-
+        "/qq": "![/qq](https://cdn.luogu.com.cn/upload/pic/62224.png)",
+        "/cy": "![/cy](https://cdn.luogu.com.cn/upload/pic/62225.png)",
+        "/kel": "![/kel](https://cdn.luogu.com.cn/upload/pic/62226.png)",
+        "/kk": "![/kk](https://cdn.luogu.com.cn/upload/pic/62227.png)",
+        "/dk": "![/dk](https://cdn.luogu.com.cn/upload/pic/62228.png)",
+        "/xyx": "![/xyx](https://cdn.luogu.com.cn/upload/pic/62230.png)",
+        "/jk": "![/jk](https://cdn.luogu.com.cn/upload/pic/62234.png)",
+        "/qiang": "![/qiang](https://cdn.luogu.com.cn/upload/pic/62236.png)",
+        "/ruo": "![/ruo](https://cdn.luogu.com.cn/upload/pic/62238.png)",
+        "/ts": "![/ts](https://cdn.luogu.com.cn/upload/pic/62239.png)",
+        "/yun": "![/yun](https://cdn.luogu.com.cn/upload/pic/62240.png)",
+        "/yiw": "![/yiw](https://cdn.luogu.com.cn/upload/pic/62243.png)",
+        "/se": "![/se](https://cdn.luogu.com.cn/upload/pic/62244.png)",
+        "/px": "![/px](https://cdn.luogu.com.cn/upload/pic/62246.png)",
+        "/wq": "![/wq](https://cdn.luogu.com.cn/upload/pic/62248.png)",
+        "/fad": "![/fad](https://cdn.luogu.com.cn/upload/pic/62250.png)",
+        "/youl": "![/youl](https://cdn.luogu.com.cn/upload/pic/69020.png)"
     };
+    const prefix = "{", suffix = "}";
     document.addEventListener("keypress", function () {
         let sourceString = markdownPalettes.content;
         for (let i in replaceElement) {
-            sourceString = sourceString.replaceAll(i, replaceElement[i]);
+            let newString = prefix + i + suffix;
+            sourceString = sourceString.replaceAll(newString, replaceElement[i]);
         }
         markdownPalettes.content = sourceString;
     })
